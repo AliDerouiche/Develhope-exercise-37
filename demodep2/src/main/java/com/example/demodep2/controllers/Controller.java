6674872d306434d73aa9717ad64a1a4053105da2
Bisco <alidero1605@gmail.com>
2024-05-27 11:47:51 +0200

    update

--- demodep2/src/main/java/com/example/demodep2/controllers/Controller.java
+++ demodep2/src/main/java/com/example/demodep2/controllers/Controller.java
@@ -4,12 +4,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Random;
+
 @RestController
-@RequestMapping("/developer")
+@RequestMapping("/calculate")
 public class Controller {
 
-    @GetMapping("/name")
-    public String getName() {
-        return "Mario";
+    @GetMapping("/sum/random")
+    public int getSum() {
+        Random random = new Random();
+        int num1 = random.nextInt(100);
+        int num2 = random.nextInt(100);
+        return num1 + num2;
     }
 }
